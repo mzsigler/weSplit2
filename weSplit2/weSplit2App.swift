a119bf073f27f070fbee45503310dde01a8aa2a5
Mikel Sigler <mzsigler@gmail.com>
2024-01-23 13:28:34 -0600

    Initial Commit

---
+++ weSplit2/weSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  weSplit2App.swift
+//  weSplit2
+//
+//  Created by Mikel Sigler on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct weSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
