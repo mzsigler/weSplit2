648daaf63b0828b57e4701a2eca374f258cee5db
Mikel Sigler <mzsigler@gmail.com>
2024-01-26 13:17:59 -0600

    Basics of the app added

--- weSplit2/ContentView.swift
+++ weSplit2/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Form {
+        NavigationStack {
+            Form {
             Section {
-                    Text("Hello, world!")
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                Picker("Number of People", selection: $numberOfPeople){
+                    ForEach(2..<100){
+                        Text("\($0) people")
+                    }
                 }
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
+            .navigationTitle("We Spilt")
+    }
     }
 }
 
